edea3117d86c4fba03bfdfe7e25b724ca1b3fada
Arash <arash.m.gholinejad@gmail.com>
2026-09-24 23:16:40 -0700

    API: fix account serializers and the verification-mail helper
    - Signup no longer lets the client set is_email_verified; password is write-only and validated
    - Reset/change password run Django password validators
    - Profile serializer used fields that do not exist (zip_code, phone_no)
    - get_unique_code() was called without its argument; codes are now random strings
    - New issue_email_verification() saves the code and mails a link that actually resolves

--- api/api_account/serializers.py
+++ api/api_account/serializers.py
@@ -1,54 +1,72 @@
 from rest_framework import serializers
 from account.models import User
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth.password_validation import validate_password
 
 
 class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, style={'input_type': 'password'})
+
     class Meta:
         model = User
-        fields = ['email', 'password', 'is_email_verified', ]
+        fields = ['id', 'email', 'username', 'password', 'first_name', 'last_name', 'is_email_verified', ]
+        read_only_fields = ['id', 'is_email_verified']
+
+    def validate(self, data):
+        validate_password(data['password'], User(email=data.get('email'), username=data.get('username')))
+        return data
 
     def create(self, validated_data):
         """ Creates and returns a new user """
-
-        # Validating Data
-        user = User(
-        email=validated_data['email'],
-        password=validated_data['password'],)
-        user.set_password(validated_data['password'])
-        user.save()
-
-        return user
+        return User.objects.create_user(**validated_data)
 
 
 class ResetPasswordSerializer(serializers.Serializer):
-    
-    new_password = serializers.CharField(required=True)
-    confirmed_password = serializers.CharField(required=True)
+
+    new_password = serializers.CharField(required=True, write_only=True)
+    confirmed_password = serializers.CharField(required=True, write_only=True)
 
     def validate(self, data):
         if data['new_password'] != data['confirmed_password']:
-            raise serializers.ValidationError("passwords should match")
+            raise serializers.ValidationError({'confirmed_password': 'passwords should match'})
+        validate_password(data['new_password'], self.context.get('user'))
         return data
 
 
 
 class ChangePasswordSerializer(serializers.Serializer):
-    old_password = serializers.CharField(required=True)
-    new_password = serializers.CharField(required=True)
-    confirmed_password = serializers.CharField(required=True)
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True)
+    confirmed_password = serializers.CharField(required=True, write_only=True)
 
     def validate(self, data):
         user = self.context['request'].user
-        if not check_password(data['old_password'], user.password):
+        if not user.check_password(data['old_password']):
             raise serializers.ValidationError({'old_password': 'Wrong password.'})
         if data['old_password'] == data['new_password']:
             raise serializers.ValidationError({'new_password': 'new password should not match with old password'})
         if data['new_password'] != data['confirmed_password']:
             raise serializers.ValidationError({'new_password': 'Passwords do not match.'})
+        validate_password(data['new_password'], user)
         return data
-    
+
+
+class EmailSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    redirect_url = serializers.CharField(required=False, allow_blank=True, default='')
+
+
+class LogoutSerializer(serializers.Serializer):
+    refresh = serializers.CharField()
+
+
 class UpdateProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['email', 'first_name','last_name','zip_code','address','phone_no', ]
+        fields = ['id', 'email', 'username', 'first_name', 'last_name', 'zipcode', 'address', 'city', 'phone', 'is_email_verified', ]
+        read_only_fields = ['id', 'is_email_verified']
+
+    def update(self, instance, validated_data):
+        # A new address has to be verified again.
+        if 'email' in validated_data and validated_data['email'] != instance.email:
+            instance.is_email_verified = False
+        return super().update(instance, validated_data)
--- api/api_account/utils.py
+++ api/api_account/utils.py
@@ -1,26 +1,40 @@
-from django.core.mail import send_mail, EmailMessage
+from django.core.mail import EmailMessage
 from django.conf import settings
-from account.models import User
+from django.urls import reverse
+from django.utils import timezone
 from django.utils.crypto import get_random_string
-import hashlib
-from datetime import datetime
-
+from account.models import EmailOTP
 
 
 def send_email(data):
-    # subject = "Verify your email"
-    # message = f"Please verify your email using this link \n\n\n 127.0.0.1:8000/account/verify-email/{otp}"
-    # email_from = settings.EMAIL_HOST
-    # send_mail(subject,message, email_from, [email])
     email = EmailMessage(
-        data['email_subject'], 
+        data['email_subject'],
         data['email_body'] ,
-        settings.EMAIL_HOST, 
+        settings.DEFAULT_FROM_EMAIL,
         data['to_emails'],
     )
     email.send()
 
-def get_unique_code(user):
-    message = user.email + str(datetime.now())
-    hash_object = hashlib.sha256(message.encode())
-    return hash_object.hexdigest()
+def get_unique_code():
+    return get_random_string(64)
+
+
+def issue_email_verification(request, user, redirect_url=''):
+    """Creates (or refreshes) the user's verification code and mails them the link."""
+    email_otp, _ = EmailOTP.objects.update_or_create(
+        user=user,
+        defaults={
+            'email_verification_code': get_unique_code(),
+            'expiration_date': timezone.now() + timezone.timedelta(days=7),
+        },
+    )
+
+    absurl = request.build_absolute_uri(reverse('api-verify-email', kwargs={'token': email_otp.email_verification_code}))
+    if redirect_url:
+        absurl += '?redirect_url=' + redirect_url
+    send_email({
+        'email_body': 'Hello, \n Use link below to verify your email  \n' + absurl,
+        'to_emails': [user.email],
+        'email_subject': 'Verify your email',
+    })
+    return email_otp
